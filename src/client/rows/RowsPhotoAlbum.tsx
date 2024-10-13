441a7a92362639593352f1dbff91404892d928b1
Siddhartha Maity <siddharthamaity1993@gmail.com>
2024-10-13 17:39:13 +0530

    perf: rows layout performance improvement

--- src/client/rows/RowsPhotoAlbum.tsx
+++ src/client/rows/RowsPhotoAlbum.tsx
@@ -1,31 +1,66 @@
-import { forwardRef, useMemo } from "react";
+import { forwardRef, useEffect, useRef, useState } from "react";
 
 import { useContainerWidth } from "../hooks";
 import StaticPhotoAlbum from "../../static";
 import resolveRowsProps from "./resolveRowsProps";
 import computeRowsLayout from "../../layouts/rows";
-import { ElementRef, ForwardedRef, JSXElement, Photo, RowsPhotoAlbumProps } from "../../types";
+import { ElementRef, ForwardedRef, JSXElement, LayoutModel, Photo, RowsPhotoAlbumProps } from "../../types";
 
 function RowsPhotoAlbum<TPhoto extends Photo>(
   { photos, breakpoints, defaultContainerWidth, ...rest }: RowsPhotoAlbumProps<TPhoto>,
   ref: ForwardedRef,
 ) {
   const { containerRef, containerWidth } = useContainerWidth(ref, breakpoints, defaultContainerWidth);
+  const [layoutModel, setLayoutModel] = useState<LayoutModel<TPhoto> | undefined>(undefined);
+  const prevPhotosCount = useRef(0);
 
   const { spacing, padding, targetRowHeight, minPhotos, maxPhotos, ...restProps } = resolveRowsProps(containerWidth, {
     photos,
     ...rest,
   });
 
-  const model = useMemo(
-    () =>
-      containerWidth !== undefined && spacing !== undefined && padding !== undefined && targetRowHeight !== undefined
-        ? computeRowsLayout(photos, spacing, padding, containerWidth, targetRowHeight, minPhotos, maxPhotos)
-        : undefined,
-    [photos, spacing, padding, containerWidth, targetRowHeight, minPhotos, maxPhotos],
-  );
+  useEffect(() => {
+    if (
+      containerWidth !== undefined &&
+      spacing !== undefined &&
+      padding !== undefined &&
+      targetRowHeight !== undefined
+    ) {
+      setLayoutModel((prevState) => {
+        const rowsLayout = computeRowsLayout(
+          photos?.slice(prevPhotosCount.current),
+          spacing,
+          padding,
+          containerWidth,
+          targetRowHeight,
+          minPhotos,
+          maxPhotos,
+        );
+        prevPhotosCount.current = photos.length;
 
-  return <StaticPhotoAlbum layout="rows" ref={containerRef} model={model} {...restProps} />;
+        return prevState === undefined
+          ? rowsLayout
+          : { ...prevState, tracks: [...prevState.tracks, ...(rowsLayout?.tracks ?? [])] };
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [photos]);
+
+  useEffect(() => {
+    if (
+      containerWidth !== undefined &&
+      spacing !== undefined &&
+      padding !== undefined &&
+      targetRowHeight !== undefined
+    ) {
+      setLayoutModel(
+        computeRowsLayout(photos, spacing, padding, containerWidth, targetRowHeight, minPhotos, maxPhotos),
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [spacing, padding, targetRowHeight, minPhotos, maxPhotos]);
+
+  return <StaticPhotoAlbum layout="rows" ref={containerRef} model={layoutModel} {...restProps} />;
 }
 
 export default forwardRef(RowsPhotoAlbum) as <TPhoto extends Photo>(
